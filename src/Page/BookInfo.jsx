22795233dc69792aa245e4951d7813f65853412b
Rayson <raysonthow@gmail.com>
2022-07-29 11:03:40 +0800

    init commit

--- src/Page/BookInfo.jsx
+++ src/Page/BookInfo.jsx
@@ -6,7 +6,7 @@ import Price from "../components/ui/Price";
 import Book from "../components/ui/Book";
 
 
-const BookInfo = ({ books, addToCart , cart }) => {
+const BookInfo = ({ books, addToCart , cart}) => {
     const {id} = useParams()
     const book = books.find((book) => +book.id === +id)
 
